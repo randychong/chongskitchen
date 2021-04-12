f5f91040e8f3e83c32907bfc9ade4b63bed4c6db
randy chong <randychong27@yahoo.com>
2021-04-12 15:47:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const recipeContainer = document.querySelector(".recipe-container");
+const searchButton = document.querySelector(".search-button");
+
+const getRecipe = async () => {
+    const search = document.querySelector(".search-input").value;
+    let data = await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${search}`, {
+        "headers": {
+            "x-rapidapi-key": "0eb660dc97msh0b6247972cec6a3p1c8277jsn29d8b3adfdd2",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }
+    })
+    let json = await data.json();
+    counter = 0;
+    for (result of json.results) {
+        const img = document.createElement("img");
+        img.src = `https://spoonacular.com/recipeImages/${json.results[`${counter}`].id}-480x360.jpg`;
+        img.className = "recipe-image";
+
+        const url = document.createElement("a");
+        url.innerHTML = json.results[`${counter}`].title;
+        url.href = json.results[`${counter}`].sourceUrl;
+        url.className = "link";
+
+        const recipeCard = document.createElement("div");
+        recipeCard.className = "recipe-card";
+
+        const urlContainer = document.createElement("p");
+        
+        urlContainer.append(url);
+        recipeCard.append(img, urlContainer);
+        recipeContainer.append(recipeCard);
+        counter += 1;
+    }
+}
+
+searchButton.addEventListener("click", ()=> {
+    if (recipeContainer != "") {
+        recipeContainer.innerHTML = "";
+        getRecipe();
+    }   else {
+        getRecipe();
+    }
+});
